3f5aa2f25d4778594711cb627e606b04ef20bf70
hey38wh37dy3 <108616172+hey38wh37dy3@users.noreply.github.com>
2022-12-13 13:53:36 +0000

    Update start.sh

--- main/start.sh
+++ main/start.sh
@@ -1,5 +1,5 @@
 export PORT=${PORT-8080}
-export UUID=${UUID-1eb6e917774b4a84aff6b058577c60a5}
+export UUID=${UUID-cae53f468058441e8c8118d05d9c0e19}
 export PATH_vless=${PATH_vless-/vless/$UUID}
 export PATH_trojan=${PATH_trojan-/trojan/$UUID}
 export PATH_vmess=${PATH_vmess-/vmess/$UUID}
